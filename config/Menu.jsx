f228feb960689e165c24d28386e9be4e7b3c8df5
JuniYadi <juniyadi94@gmail.com>
2020-08-26 13:16:36 +0700

    Update Config Menu

--- config/Menu.jsx
+++ config/Menu.jsx
@@ -1,28 +1,51 @@
 export const menuTop = [
-    { name: 'Home', href: '/'},
-    { name: 'Jasa', href: '#', child: [
-        { name: 'Jual Beli Saldo Paypal', href: 'https://kedaiexchange.com/', eksternal: true },
-        { name: 'Whatsapp Gateway', href: '/product/whatsapp-gateway' },
-    ]},
-    { name: 'Server', href: '#', child: [
-        { name: 'RDP User', href: '/product/rdp-user' },
-        { name: 'RDP Emulator', href: '/product/rdp-emulator' },
-        { name: 'RDP Dedicated', href: '/product/rdp-dedicated' },
-        { name: 'KVM VPS Canada', href: '/product/kvm-vps-canada' },
-        { name: 'KVM VPS France', href: '/product/kvm-vps-france' },
-        { name: 'KVM VPS Singapore', href: '/product/kvm-vps-singapore' },
-        { name: 'VPN', href: '/product/vpn' },
-    ]},
-    { name: 'Seedbox', href: '#', child: [
-        { name: 'Leechbox', href: '/product/leechbox' },
-        { name: 'Seedbox', href: '/product/seedbox' },
-        { name: 'Seedbox Dedicated', href: '/product/seedbox-dedicated' },
-        { name: 'Google Drive Mirror', href: '/product/google-drive-mirror' },
-    ]},
-    { name: 'Pembayaran', href: '#', child: [
-        { name: 'Metode Pembayaran', href: '/payment-method' },
-        { name: 'Konfirmasi Pembayaran', href: 'https://pay.premiumfast.net/', eksternal: true },
-    ]},
+    { name: 'Home', href: '/' },
+    {
+        name: 'Jasa',
+        href: '#',
+        child: [
+            {
+                name: 'Jual Beli Saldo Paypal',
+                href: 'https://kedaiexchange.com/',
+                eksternal: true,
+            },
+            { name: 'Whatsapp Gateway', href: '/product/whatsapp-gateway' },
+        ],
+    },
+    {
+        name: 'Server',
+        href: '#',
+        child: [
+            { name: 'RDP User', href: '/product/rdp-user' },
+            { name: 'RDP Emulator', href: '/product/rdp-emulator' },
+            { name: 'RDP Dedicated', href: '/product/rdp-dedicated' },
+            { name: 'KVM VPS Canada', href: '/product/kvm-vps-canada' },
+            { name: 'KVM VPS France', href: '/product/kvm-vps-france' },
+            { name: 'KVM VPS Germany', href: '/product/kvm-vps-germany' },
+            { name: 'KVM VPS Singapore', href: '/product/kvm-vps-singapore' },
+            { name: 'VPN', href: '/product/vpn' },
+        ],
+    },
+    {
+        name: 'Seedbox',
+        href: '#',
+        child: [
+            { name: 'Leechbox', href: '/product/leechbox' },
+            { name: 'Seedbox', href: '/product/seedbox' },
+        ],
+    },
+    {
+        name: 'Pembayaran',
+        href: '#',
+        child: [
+            { name: 'Metode Pembayaran', href: '/payment-method' },
+            {
+                name: 'Konfirmasi Pembayaran',
+                href: 'https://pay.premiumfast.net/',
+                eksternal: true,
+            },
+        ],
+    },
 ];
 
 export const menuBottom = [];
